69e19848d0fc6c8ac82fa6d1774d97c788b8d846
piplongrun <pip.longrun@yahoo.com>
2014-12-06 18:52:39 +0100

    Use Resource.ContentsOfURLWithFallback

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,93 +1,56 @@
-NAME = 'beeg'
-BASE_URL = 'http://beeg.com'
-SECTION_URL = 'http://beeg.com/section/%s/%%d/'
-TAG_URL = 'http://beeg.com/tag/%s/%%d/'
 THUMB_URL = 'http://cdn.anythumb.com/640x360/%s.jpg'
 THUMB_URL_ORIG = 'http://cdn.anythumb.com/236x177/%s.jpg'
-
-ART = 'art-default.jpg'
-ICON = 'icon-default.jpg'
-
-RE_VIDEO_IDS = Regex('var tumbid.*=.*\[(.+?)\];')
-RE_TITLES = Regex('var tumbalt.*=.*\[(.+?)\];')
+RE_VIDEO_URL = Regex("'file': '([^']+)'")
 
 ####################################################################################################
-def Start():
+def NormalizeURL(url):
 
-	ObjectContainer.art = R(ART)
-	ObjectContainer.title1 = NAME
-	DirectoryObject.thumb = R(ICON)
-
-	HTTP.CacheTime = CACHE_1HOUR * 4
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25'
+	return url.split('?')[0].rstrip('/')
 
 ####################################################################################################
-@handler('/video/beeg', NAME, thumb=ICON, art=ART)
-def MainMenu():
+def MetadataObjectForURL(url):
 
-	oc = ObjectContainer()
+	html = HTML.ElementFromURL(url)
 
-	oc.add(DirectoryObject(
-		key = Callback(Videos, title='Browse All Videos', url=SECTION_URL % 'home'),
-		title = 'Browse All Videos'
-	))
+	title = html.xpath('//title/text()')[0].strip()
+	summary = html.xpath('//meta[@name="description"]/@content')[0].rsplit(' - free', 1)[0].strip()
+	thumb_1 = THUMB_URL % url.split('/')[-1]
+	thumb_2 = THUMB_URL_ORIG % url.split('/')[-1]
 
-	oc.add(DirectoryObject(
-		key = Callback(Tags, title='Browse Videos by Tag'),
-		title = 'Browse Videos by Tag'
-	))
-
-	return oc
-
-####################################################################################################
-@route('/video/beeg/videos', page=int)
-def Videos(title, url, page=1):
-
-	oc = ObjectContainer(title2=title)
-	page = HTTP.Request(url % page).content
-
-	ids = RE_VIDEO_IDS.search(page).group(1).split(",")[0:50]
-	titles = RE_TITLES.search(page).group(1).strip("'").split("','")[0:50]
-
-	for i, id in enumerate(ids):
-		oc.add(VideoClipObject(
-			url = '%s/%s' % (BASE_URL, id),
-			title = titles[i].decode('string_escape'),
-			thumb = Callback(GetThumb, id=id)
-		))
-
-	return oc
+	return VideoClipObject(
+		title = title,
+		summary = summary,
+		thumb = Resource.ContentsOfURLWithFallback([thumb_1, thumb_2], fallback='icon-default.jpg'),
+		content_rating = 'X'
+	)
 
 ####################################################################################################
-@route('/video/beeg/tags')
-def Tags(title):
+def MediaObjectsForURL(url):
 
-	oc = ObjectContainer(title2=title)
-
-	for tag in HTML.ElementFromURL(BASE_URL).xpath('//a[contains(@href, "/tag/")]'):
-		title = tag.xpath('./text()')[0]
-		href = tag.xpath('./@href')[0].split('/')[-1].replace('%', '%%')
-
-		oc.add(DirectoryObject(
-			key = Callback(Videos, title=title, url=TAG_URL % href),
-			title = title
-		))
-
-	oc.objects.sort(key=lambda obj: obj.title)
-
-	return oc
+	return [
+		MediaObject(
+			container = Container.MP4,
+			video_codec = VideoCodec.H264,
+			video_resolution = '480',
+			audio_codec = AudioCodec.AAC,
+			audio_channels = 2,
+			optimized_for_streaming = True if Client.Product not in ['Plex Web'] else False,
+			parts = [
+				PartObject(
+					key = Callback(PlayVideo, url=url)
+				)
+			]
+		)
+	]
 
 ####################################################################################################
-def GetThumb(id):
+@indirect
+def PlayVideo(url):
 
-	try:
-		data = HTTP.Request(THUMB_URL % id, cacheTime=CACHE_1MONTH, sleep=0.5).content
-		return DataObject(data, 'image/jpeg')
-	except:
-		try:
-			data = HTTP.Request(THUMB_URL_ORIG % id, cacheTime=CACHE_1MONTH, sleep=0.5).content
-			return DataObject(data, 'image/jpeg')
-		except:
-			pass
+	data = HTTP.Request(url).content
+	video = RE_VIDEO_URL.search(data)
 
-	return Redirect(R(ICON))
+	if video:
+		return IndirectResponse(VideoClipObject, key=video.group(1))
+
+	raise Ex.MediaNotAvailable
